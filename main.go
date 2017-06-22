f1b1e2c2081161dadc4f3e3e596b3bc00897d804
Shintaro Kojima <goodies@codeout.net>
2017-06-23 02:24:19 +0900

    Split large function into another file

--- main.go
+++ main.go
@@ -1,11 +1,9 @@
 package main
 
 import (
-	"encoding/json"
 	"flag"
 	"fmt"
 	"github.com/codeout/inetb/client"
-	"io/ioutil"
 	"log"
 	"sync"
 	"time"
@@ -19,77 +17,6 @@ type Report struct {
 
 func (r Report) String() string {
 	return fmt.Sprintf("%s, Sent: %d, Received: %d", r.Time, r.Sent, r.Received)
-}
-
-func advertiseNewRoutes(client1 *client.Client, client2 *client.Client) error {
-	log.Print("Start benchmarking - Advertise new routes from client1")
-
-	if err := client1.DeprefExport(); err != nil {
-		log.Fatal(err)
-	}
-	if err := client1.AcceptExport(); err != nil {
-		log.Fatal(err)
-	}
-
-	reports := make([]*Report, 0)
-	sent := 0
-	received := 0
-	timeout := 60
-
-	for tick := 0; tick < timeout; tick++ {
-		func() {
-			for {
-				select {
-				case update := <-client1.Updates:
-					if routerId, _ := client1.RouterId(); update.Nexthop == routerId {
-						sent += len(update.Raw.NLRI)
-					}
-					tick = 0
-				default:
-					return
-				}
-			}
-		}()
-
-		func() {
-			for {
-				select {
-				case update := <-client2.Updates:
-					if routerId, _ := client1.RouterId(); update.Nexthop != routerId {
-						received += len(update.Raw.NLRI)
-					}
-					tick = 0
-				default:
-					return
-				}
-			}
-		}()
-
-		report := &Report{
-			Time:     time.Now().Format("15:04:05"),
-			Sent:     sent,
-			Received: received,
-		}
-
-		log.Print(report.String())
-		reports = append(reports, report)
-
-		if sent == 0 && received == 0 {
-			tick = 0
-		}
-
-		time.Sleep(time.Second)
-	}
-
-	log.Print("Stop benchmarking - Advertise new routes from client1")
-
-	json, err := json.Marshal(reports)
-	if err != nil {
-		return err
-	}
-
-	ioutil.WriteFile("advertise_new_routes.json", json, 0644)
-	return err
 }
 
 func main() {
